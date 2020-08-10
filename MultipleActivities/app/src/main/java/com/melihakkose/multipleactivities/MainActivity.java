2aaf2f95526d94e82a247bbc07740b3d862afab0
Melih <Olymposrec@users.noreply.github.com>
2020-08-10 21:54:51 +0300

    Android Studios Multiple Activities
    Android Studios Multiple Activities işlemine bakacağız.

---
+++ MultipleActivities/app/src/main/java/com/melihakkose/multipleactivities/MainActivity.java
@@ -0,0 +1,31 @@
+package com.melihakkose.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    String username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.editText);
+        username="";
+    }
+
+    public void  changeActivity(View view){
+        username=editText.getText().toString();
+        Intent intent =new Intent(MainActivity.this,MainActivity2.class);
+        intent.putExtra("userInput",username);
+        startActivity(intent);
+
+
+
+    }
+}
